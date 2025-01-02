a66636fc3a16db1418664251e54d01f0492f0f27
Bruna Trombetta <brunatrombetta9@gmail.com>
2025-01-02 03:43:48 -0300

    feta: OrderStatus enumeration

--- src/main/java/com/example/teste/entities/Order.java
+++ src/main/java/com/example/teste/entities/Order.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.example.teste.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,9 +23,12 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long order_id;
+	
+	@JsonFormat (shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
-	@JsonIgnore
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn (name = "client_id")
 	private User client;
@@ -33,9 +37,10 @@ public class Order implements Serializable {
 		
 	}
 	
-	public Order(Instant moment, User client) {
+	public Order (Instant moment, OrderStatus orderStatus, User client) {
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 		
 	}
 
@@ -53,6 +58,17 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+			
 	}
 
 	public User getClient() {
